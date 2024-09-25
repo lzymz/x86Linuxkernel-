6fa8a89c6439b20ef380ad561b601901c161e0e9
“liuxin” <liuxin152830@163.com>
2024-09-25 20:10:49 +0800

    V1.2

---
+++ source/kernel/include/fs/devfs/devfs.h
@@ -0,0 +1,19 @@
+/**
+ * 设备文件系统描述
+ *
+ */
+#ifndef DEVFS_H
+#define DEVFS_H
+
+#include "fs/fs.h"
+
+/**
+ * @brief 设备类型描述结构
+ */
+typedef struct _devfs_type_t {
+    const char * name;
+    int dev_type;
+    int file_type;
+}devfs_type_t;
+
+#endif
---
+++ source/kernel/include/tools/log.h
@@ -0,0 +1,10 @@
+/**
+ * 日志输出
+ */
+#ifndef LOG_H
+#define LOG_H
+
+void log_init (void);
+void log_printf(const char * fmt, ...);
+
+#endif // LOG_H
